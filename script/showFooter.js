9899627a1ecef244cc71ad0ff68fe0d7c4664785
oistream <62792133+zxp2019@users.noreply.github.com>
2020-08-20 15:10:03 +0800

    修复了一个 Bug

--- script/showFooter.js
+++ script/showFooter.js
@@ -1,4 +1,4 @@
-var version = "4.10.0"
+var version = "4.11.0"
 var years = "2020"
-var date = "Aug 19th,2020"
-document.write('<p style="font-family:Consolas;color:white;">OIstreamblog ' + version + ' | Designed by (©) ' + years + ' OIstream & ASFOS | 更新于 ' + date + ' | <a href="malito:zxp1863@163.com" style="color:white;">联系我们</a>（zxp1863@163.com）' + ' | <a href="https://zxp2019.github.io/new.txt" style="color:white;">更新记录</a> | 背景是由 <a href="https://bing.com/>bing.com</a> 提供的随机壁纸</p>')
+var date = "Aug 20th,2020"
+document.write('<p style="font-family:Consolas;color:white;">OIstreamblog ' + version + ' | Designed by (©) ' + years + ' OIstream & ASFOS | 更新于 ' + date + ' | <a href="malito:zxp1863@163.com" style="color:white;">联系我们</a>（zxp1863@163.com）' + ' | <a href="https://zxp2019.github.io/new.txt" style="color:white;">更新记录</a> | 背景是由 <a href="https://bing.com/">bing.com</a> 提供的随机壁纸</p>')
