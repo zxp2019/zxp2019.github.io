8a5dfa325892d3dc4dc82130133c7e8b6a590778
oistream <62792133+zxp2019@users.noreply.github.com>
2020-08-20 16:15:47 +0800

    增加了对新功能的支持

--- script/blog.js
+++ script/blog.js
@@ -6,3 +6,19 @@ function showList()
 {
     document.getElementById("divlist").style=""
 }
+function hideTag()
+{
+    var arr=document.getElementsByClassName("ttag")
+    for(i=0;i<arr.length;i++)
+    {
+        arr[i].style="display:none;"
+    }
+}
+function showTag()
+{
+    var arr=document.getElementsByClassName("ttag")
+    for(i=0;i<arr.length;i++)
+    {
+        arr[i].style=""
+    }
+}
