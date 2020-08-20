9b06b0ae3a4b800645268a7b2a2b96a81cce8e9d
oistream <62792133+zxp2019@users.noreply.github.com>
2020-08-20 15:22:21 +0800

    Update showFooter.js

--- script/showFooter.js
+++ script/showFooter.js
@@ -1,4 +1,4 @@
 var version = "4.11.0"
 var years = "2020"
 var date = "Aug 20th,2020"
-document.write('<p style="font-family:Consolas;color:white;">OIstreamblog ' + version + ' | Designed by (©) ' + years + ' OIstream & ASFOS | 更新于 ' + date + ' | <a href="malito:zxp1863@163.com" style="color:white;">联系我们</a>（zxp1863@163.com）' + ' | <a href="https://zxp2019.github.io/new.txt" style="color:white;">更新记录</a> | 背景是由 <a href="https://bing.com/">bing.com</a> 提供的随机壁纸</p>')
+document.write('<p style="font-family:Consolas;color:black;">OIstreamblog ' + version + ' | Designed by (©) ' + years + ' OIstream & ASFOS | 更新于 ' + date + ' | <a href="malito:zxp1863@163.com">联系我们</a>（zxp1863@163.com）' + ' | <a href="https://zxp2019.github.io/new.txt">更新记录</a> | 背景是随机的由 <a href="https://bing.com/">bing.com</a> 提供的壁纸</p><br/>')
