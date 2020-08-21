92d51bbc51ecaac828da5f4b3d64e4c34f589ca1
oistream <62792133+zxp2019@users.noreply.github.com>
2020-08-21 10:47:29 +0800

    增加对【工具箱】功能的支持

---
+++ script/showTool.js
@@ -0,0 +1,18 @@
+document.write('<div id="toolBox"><div id="toolBoxUp"><div style="float:right;"><button onclick="hideToolBox()"><i class="fa fa-close"></i></button></div></div><div id="toolBoxMain"><button class="newButton" title="不喜欢这张背景图？点击换一张" onclick="refresh()"><i class="fa fa-refresh"></i></button><button class="newButton" title="喜欢这张背景图？点击全屏显示" onclick="showBgImg()" id="needChange"><i class="fa fa-expand"></i></button></div></div><div id="toolBoxHide" style="display:none;"><div style="text-align:center;"><button onclick="showToolBox()"><i class="fa fa-angle-up"></i></button></div></div>')
+/*
+<div id="toolBox">
+		<div id="toolBoxUp">
+			<div style="float:right;">
+				<button onclick="hideToolBox()"><i class="fa fa-close"></i></button>
+			</div>
+		</div>
+		<div id="toolBoxMain">
+			<button class="newButton" title="不喜欢这张背景图？点击换一张" onclick="refresh()"><i class="fa fa-refresh"></i></button><button class="newButton" title="喜欢这张背景图？点击全屏显示" onclick="showBgImg()" id="needChange"><i class="fa fa-expand"></i></button>
+		</div>
+	</div>
+	<div id="toolBoxHide" style="display:none;">
+		<div style="text-align:center;">
+			<button onclick="showToolBox()"><i class="fa fa-angle-up"></i></button>
+		</div>
+    </div>
+*/
