bbe3d2f007ba2b78f0a98d3f4ad854727fec0d12
oistream <62792133+zxp2019@users.noreply.github.com>
2020-08-21 10:48:12 +0800

    增加了对【工具箱】功能的支持

--- script/blog.js
+++ script/blog.js
@@ -22,3 +22,31 @@ function showTag()
         arr[i].style=""
     }
 }
+function hideToolBox()
+{
+    document.getElementById("toolBox").style="display:none;"
+    document.getElementById("toolBoxHide").style=""
+}
+function showToolBox()
+{
+    document.getElementById("toolBox").style=""
+    document.getElementById("toolBoxHide").style="display:none;"
+}
+function refresh()
+{
+    window.location.reload(true)
+}
+function showBgImg()
+{
+    document.getElementById("container").style="display:none;"
+    document.getElementById("needChange").innerHTML='<i class="fa fa-compress"></i>'
+    document.getElementById("needChange").setAttribute("onclick","hideBgImg()")
+    document.getElementById("needChange").title="退出全屏显示"
+}
+function hideBgImg()
+{
+    document.getElementById("container").style=""
+    document.getElementById("needChange").innerHTML='<i class="fa fa-expand"></i>'
+    document.getElementById("needChange").setAttribute("onclick","showBgImg()")
+    document.getElementById("needChange").title="喜欢这张背景图？点击全屏显示"
+}
