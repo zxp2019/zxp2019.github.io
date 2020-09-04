fa68d7bd5278def2318fbd8b70a9880a77b2339b
oistream <62792133+zxp2019@users.noreply.github.com>
2020-09-04 19:16:51 +0800

    new project: OIerHub

---
+++ oierhub/script/main.js
@@ -0,0 +1,22 @@
+var menu=[
+    {
+        text:"首页",
+        href:""
+    },
+    {
+        text:"OIstream",
+        href:"https://zxp2019.github.io/"
+    }
+]
+
+function showMenu()
+{
+    document.write('<div id="menu">')
+    document.write('<ul>')
+    for(i=0;i<menu.length;i++)
+    {
+        document.write('<li><a href="'+menu[i].href+'">'+menu[i].text+'</a></li>')
+    }
+    document.write('</ul>')
+    document.write('</div>')
+}
