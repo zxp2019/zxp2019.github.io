fc8e3c94883c8c17dcf98a9eb58a5eb9740e8321
oistream <62792133+zxp2019@users.noreply.github.com>
2020-11-06 20:13:17 +0800

    v5.0.1 : 分离数据与操作

---
+++ script/data-v5.js
@@ -0,0 +1,30 @@
+var faicons = {
+    "tag" : "fa fa-tag", 
+    "knowledge" : "fa fa-book",
+    "postDate" : "fa fa-clock-o", 
+    "updateDate" : "fa fa-check-square-o", 
+    "language" : "fa fa-file"
+};
+
+var articles = [
+    {
+        title : "Not Found",
+        url : "", 
+        tags : ["Undefined"], 
+        knowledges : ["Undefined"], 
+        postDate : "Undefined", 
+        updateDate : "Undefined", 
+        language : "", 
+        description : "出现了一些错误 qwq 请联系博主解决问题。错误代码：ERR_ARTICLE_0_INDEX。"
+    }, 
+    {
+        title : "「置顶」新版博客测试",
+        url : "blog-v4-test", 
+        tags : ["GMQ", "AK", "IOI"], 
+        knowledges : ["其它"], 
+        postDate : "2020-05-16 14:32", 
+        updateDate : "2020-05-16 14:32", 
+        language : "C++", 
+        description : "快来看看新版博客长什么样吧！"
+    }, 
+];
