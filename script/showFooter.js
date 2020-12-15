9a64d84766160c362ecb1a8ffaf7d2be9ca085de
oistream <62792133+zxp2019@users.noreply.github.com>
2020-12-15 22:29:04 +0800

    Thanks @kevinzhw153

--- script/showFooter.js
+++ script/showFooter.js
@@ -1,4 +1,4 @@
 var version = "5.0"
 var years = "2020"
 var date = "Dec 5th,2020"
-document.write('<p style="font-family:Consolas;color:black;">OIstreamblog ' + version + ' | Designed by (©) ' + years + ' OIstream & ASFOS | 更新于 ' + date + ' | <a href="malito:zxp1863@163.com">联系我们</a>（zxp1863@163.com）' + ' | <a href="https://zxp2019.github.io/new.txt">更新记录</a> | 背景是随机的由 <a href="https://bing.com/">bing.com</a> 提供的壁纸 | 本站所有博文的共享遵循 <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/deed.zh">CC BY-NC-ND 4.0</a> 协议</p><br/>')
+document.write('<p style="font-family:Consolas;color:black;">OIstreamblog ' + version + ' | Designed by (©) ' + years + ' OIstream & ASFOS & kevinzhw153 | 更新于 ' + date + ' | <a href="malito:zxp1863@163.com">联系我们</a>（zxp1863@163.com）' + ' | <a href="https://zxp2019.github.io/new.txt">更新记录</a> | 背景是随机的由 <a href="https://bing.com/">bing.com</a> 提供的壁纸 | 本站所有博文的共享遵循 <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/deed.zh">CC BY-NC-ND 4.0</a> 协议</p><br/>')
